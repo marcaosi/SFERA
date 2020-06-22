c29453973992a700c3e9adab53fbd9a68a750567
Marcos <marcaosi2014@gmail.com>
2020-06-22 09:28:10 -0300

    add API to CRUD saal

--- src/NovaSala/index.js
+++ src/NovaSala/index.js
@@ -1,29 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Title from '../Components/Title'
+import axios from '../Service/Axios'
+import swal from 'sweetalert'
 
 import styles from './styles.module.css'
 
 export default function NovoColaborador(){
+
+    const [data, setData] = useState({
+        ano: new Date().getFullYear(),
+        descricao: ""
+    })
+
+    const handleChangeText = (event) => {
+        setData({
+            ...data,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault()
+
+        axios
+            .post("sala", data)
+            .then(data => {
+                swal("Dados salvos com sucesso.")
+            })
+            .catch(err => {
+                swal("Falha ao salvar, verifique os dados e tente novamente.")
+            })
+    }
     return (
         <>
             <Title>Adicionar nova sala de aula</Title>
 
             <div className="row">
                 <div className="col">
-                    <form className={ styles.form }>
+                    <form className={ styles.form } onSubmit={handleSubmitForm}>
                         <div className="form-group">
                             <label>Ano</label>
-                            <input type="number" className="form-control" />
+                            <input type="number" className="form-control" name="ano" value={data.ano} onChange={handleChangeText} />
                         </div>
 
                         <div className="form-group">
                             <label>Descrição</label>
-                            <textarea className="form-control" placeholder="Berçário I"></textarea>
+                            <textarea className="form-control" name="descricao" onChange={handleChangeText} placeholder="Berçário I">{data.descricao}</textarea>
                         </div>
 
                         <div className="form-group text-center">
-                            <button className="btn btn-primary btn-lg" type="button">Salvar</button>
+                            <button className="btn btn-primary btn-lg" type="submit">Salvar</button>
                         </div>
                     </form>
                 </div>
