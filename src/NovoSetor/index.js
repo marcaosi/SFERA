6a1e480897b048446109f7ff725170f201f35366
Marcos <marcaosi2014@gmail.com>
2020-06-22 09:23:11 -0300

    add api to CRUD setor

--- src/NovoSetor/index.js
+++ src/NovoSetor/index.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Title from '../Components/Title'
+import axios from '../Service/Axios'
 
 import styles from './styles.module.css'
+import swal from 'sweetalert'
 
 export default function NovoColaborador(){
+    const [data, setData] = useState({
+        nome: ""
+    })
+
+    const handleChangeText = (event) => {
+        setData({
+            ...data,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault()
+
+        axios
+            .post("setor", data)
+            .then(data => {
+                swal("Dados salvos com sucesso.")
+            })
+            .catch(err => {
+                swal("Falha ao salvar, verifique os dados e tente novamente.")
+            })
+    }
+
     return (
         <>
             <Title>Adicionar novo setor de trabalho</Title>
 
             <div className="row">
                 <div className="col">
-                    <form className={ styles.form }>
+                    <form className={ styles.form } onSubmit={handleSubmitForm}>
                         <div className="form-group">
                             <label>Nome</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="nome" onChange={handleChangeText} />
                         </div>
 
                         <div className="form-group text-center">
-                            <button className="btn btn-primary btn-lg" type="button">Salvar</button>
+                            <button className="btn btn-primary btn-lg" type="submit">Salvar</button>
                         </div>
                     </form>
                 </div>
