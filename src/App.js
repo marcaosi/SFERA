e8b0d1c19896c7522bb245300f594d68dfb71fdf
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2020-07-01 16:19:53 -0300

    refatorando componentes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import NovoColaborador from './NovoColaborador'
 import Login from './Login'
 import PrivateRoute from './Components/PrivateRoute'
 import NovoAluno from './NovoAluno'
+import ListarSalas from './NovaSala/ListarSalas'
 
 function App() {
   return (
@@ -33,6 +34,9 @@ function App() {
                     </PrivateRoute>
                     <PrivateRoute path="/sala/novo" exact>
                       <NovaSala />
+                    </PrivateRoute>
+                    <PrivateRoute path="/sala" exact>
+                      <ListarSalas />
                     </PrivateRoute>
                     <PrivateRoute path="/colaborador/novo" exact>
                       <NovoColaborador />
--- src/Components/PrivateRoute/index.js
+++ src/Components/PrivateRoute/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Redirect, useLocation } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 export default function PrivateRoute({children, ...rest}){
     const jwt = localStorage.getItem("jwt")
