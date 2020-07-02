a659dc1938bd07ca9e5bca1156fdf80fa40e868e
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2020-07-01 21:00:38 -0300

    add routes to edit data

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import Login from './Login'
 import PrivateRoute from './Components/PrivateRoute'
 import NovoAluno from './NovoAluno'
 import ListarSalas from './NovaSala/ListarSalas'
+import ListarSetores from './NovoSetor/ListarSetores'
+import ListarColaboradores from './NovoColaborador/ListarColaboradores'
+import ListarAlunos from './NovoAluno/ListarAlunos'
 
 function App() {
   return (
@@ -32,7 +35,16 @@ function App() {
                     <PrivateRoute path="/setor/novo" exact>
                       <NovoSetor />
                     </PrivateRoute>
+                    <PrivateRoute path="/setor/:id/editar" exact>
+                      <NovoSetor />
+                    </PrivateRoute>
+                    <PrivateRoute path="/setor" exact>
+                      <ListarSetores />
+                    </PrivateRoute>
                     <PrivateRoute path="/sala/novo" exact>
+                      <NovaSala />
+                    </PrivateRoute>
+                    <PrivateRoute path="/sala/:id/editar" exact>
                       <NovaSala />
                     </PrivateRoute>
                     <PrivateRoute path="/sala" exact>
@@ -41,8 +53,20 @@ function App() {
                     <PrivateRoute path="/colaborador/novo" exact>
                       <NovoColaborador />
                     </PrivateRoute>
+                    <PrivateRoute path="/colaborador/:id/editar" exact>
+                      <NovoColaborador />
+                    </PrivateRoute>
+                    <PrivateRoute path="/colaborador" exact>
+                      <ListarColaboradores />
+                    </PrivateRoute>
                     <PrivateRoute path="/aluno/novo" exact>
                       <NovoAluno />
+                    </PrivateRoute>
+                    <PrivateRoute path="/aluno/:id/editar" exact>
+                      <NovoAluno />
+                    </PrivateRoute>
+                    <PrivateRoute path="/aluno" exact>
+                      <ListarAlunos />
                     </PrivateRoute>
                 </Content>
               </div>
